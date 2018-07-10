e64b8b975b92affcd614d44c1bea7e88b1ef4c99
n0psledbyte <n0psledbyte@gmail.com>
2018-07-10 17:23:45 +0700

    update resize.sh

--- tmux/resize.sh
+++ tmux/resize.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cl=${1}
 if [ $cl -gt 85 ] ; then
-    tmux set status-right "#[fg=colour236,bg=colour235]#[fg=colour12,bg=colour236] #(~/tmux/./cmus_status) #[fg=colour12,bg=colour236,nobold,nounderscore,noitalics]#[fg=colour235,bg=colour12] #(~/tmux/suhu)#[fg=colour236,bg=colour12] #[fg=colour12,bg=colour236] #(~/tmux/./vol) #[fg=colour12,bg=colour236,nobold,nounderscore,noitalics]#[fg=colour235,bg=colour12] #(~/tmux/./bat) #[fg=colour236,bg=colour12] #[fg=colour12,bg=colour236] %H:%M  #[fg=colour12,bg=colour236,nobold,nounderscore,noitalics]#[fg=colour235,bg=colour12] #h  "
+    tmux set -g status-right "#[fg=colour236,bg=colour235]#[fg=colour12,bg=colour236,nobold,nounderscore,noitalics]#[fg=colour235,bg=colour12] #(~/tmux/suhu)#[fg=colour236,bg=colour12] #[fg=colour12,bg=colour236] #(~/tmux/mem) #[fg=colour12,bg=colour236,nobold,nounderscore,noitalics]#[fg=colour235,bg=colour12] #(~/tmux/./bat)#[fg=colour236,bg=colour12] #[fg=colour12,bg=colour236] %H:%M ⌚ #[fg=colour4,bg=colour236,nobold,nounderscore,noitalics]#[fg=colour0,bg=colour4] #h  "
 else
     tmux set status-right ""
 fi
